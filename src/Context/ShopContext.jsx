8df27e3bcc0e56e777d168d9541c72abdd19b68c
Kartik Baruah <kbkartikbaruah1@gmail.com>
2025-01-25 11:55:01 +0530

    Update

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -6,7 +6,7 @@ export const ShopContext = createContext(null);
 const getDefaultCart = () => {
     let cart = {};
     for (let index = 0; index < all_product.length; index++) {
-        cart[index] = 0;git 
+        cart[index] = 0;
     }
     return cart;
 };
